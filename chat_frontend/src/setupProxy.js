2665ddd98fc1ee8c7881af508eef15536757b50c
Dino Babić <dinobabi3@gmail.com>
2023-12-05 21:07:57 +0100

    proxy

--- chat_frontend/src/setupProxy.js
+++ chat_frontend/src/setupProxy.js
@@ -1,10 +1,30 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
+/*module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
       target: "http://localhost:8080/",
+      changeOrigin: true,
+    })
+  );
+};*/
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    proxy({
+      target: 'http://localhost:8080/',
+      changeOrigin: true,
+    })
+  );
+
+  // Proxy WebSocket requests
+  app.use(
+    '/ws',
+    proxy({
+      target: 'http://localhost:8080/',
+      ws: true,  // Enable WebSocket proxying
       changeOrigin: true,
     })
   );
